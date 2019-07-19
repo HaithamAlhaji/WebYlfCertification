3a00f899e190f12f2cca8075f9048b32dbcabe24
Haitham Alhaji (Laptop) <haithamtalhaji@yahoo.com>
2019-07-19 03:09:40 +0300

    YLF5

--- routes/home/index.js
+++ routes/home/index.js
@@ -8,58 +8,16 @@ router.all("*", (req, res, next) => {
 router.get("/", (req, res) => {
   const defaultStyle = req.app.get("defaultStyle");
   res.render("home/index", {
-    item: "index" /* For navbar active */,
+    item: "download" /* For navbar active */,
     defaultStyle: defaultStyle
-  });
-});
-
-router.get("/categories", (req, res) => {
-  const defaultStyle = req.app.get("defaultStyle");
-  res.render("home/categories", {
-    item: "categories",
-    defaultStyle: defaultStyle
-  });
-});
-
-router.get("/book/:id", (req, res) => {
-  var id = req.params.id;
-  const defaultStyle = req.app.get("defaultStyle");
-  const mysqlConnection = req.app.get("mysqlConnection");
-  mysqlConnection.query(`CALL getBook(${id});`, (errors, results, fields) => {
-    if (errors) {
-      res.send("sql error");
-    } else if (results[0].length == 0) {
-      res.send("no book");
-    } else {
-      res.render("home/book", {
-        item: "book",
-        defaultStyle: defaultStyle,
-        book: {
-          id: results[0][0].id,
-          name: results[0][0].name,
-          description: results[0][0].description,
-          pagesCount: results[0][0].page_count,
-          category: results[0][0].categoryName,
-          author: results[0][0].author,
-          version: results[0][0].version,
-          creation: results[0][0].creation,
-          container: {
-            color: results[0][0].color
-          },
-          floor: {
-            name: results[0][0].floorName
-          }
-        }
-      });
-    }
   });
 });
 
 router.post("/download", (req, res) => {
-  if (req.body.member_name) {
+  if (req.body.member_email) {
     mysqlConnection.getConnection((err, connection) => {
-      const sql = `select 'haitham' as \`result\`,tbl_ylf_memebers.version  from tbl_ylf_memebers where name = ${mysqlConnection.escape(
-        req.body.member_name
+      const sql = `select email,version,name,serial_number from tbl_ylf_memebers WHERE email = ${mysqlConnection.escape(
+        req.body.member_email
       )};`;
 
       connection.query(sql, (errors, results, fields) => {
@@ -68,7 +26,7 @@ router.post("/download", (req, res) => {
         if (errors) {
           throw errors;
         } else {
-          if (true || (results.length > 0 && results[0].result == "haitham")) {
+          if (results.length > 0) {
             // ملتقى بابل كل الاسماء ممكن تاخذ شهادة true
             var fs = require("fs");
             var PDFDocument = require("pdfkit");
@@ -76,30 +34,41 @@ router.post("/download", (req, res) => {
             var pdf = new PDFDocument({
               size: "A4", // See other page sizes here: https://github.com/devongovett/pdfkit/blob/d95b826475dd325fb29ef007a9c1bf7a527e9808/lib/page.coffee#L69
               layout: "landscape",
+              margin: 0,
               info: {
                 Title: "Youthlf",
                 Author: "Hatiham Alhaji"
               }
             });
             const filePath = uniqueName("./public/tmp/") + ".pdf";
-            if (true || results[0].version == 0) {
-              // الانبار
+            if (results[0].version == 2) {
+              // الملتقى الخامس
               pdf
-                .font("./public/fonts/Cairo-Regular.ttf")
-                .fontSize("40")
-                .image("./public/uploads/3.png", 0, 0, { scale: 0.24 })
-                .text(
-                  req.body.member_name
-                    .toString()
-                    .split(" ")
-                    .reverse()
-                    .join(" "),
-
-                  req.body.member_name.toString().split(" ").length == 3
-                    ? 280
-                    : 200,
-                  220
+                .font("./public/fonts/FONT BOLD.otf")
+                .fontSize("23")
+                .image(
+                  results[0].member_type == 0
+                    ? "./public/uploads/4.png"
+                    : "./public/uploads/5.png",
+                  0,
+                  0,
+                  { scale: 0.24 }
                 )
+                .text(
+                  results[0].name,
+                  results[0].name.toString().split(" ").length == 1
+                    ? 300
+                    : results[0].name.toString().split(" ").length == 2
+                    ? 530
+                    : results[0].name.toString().split(" ").length == 3
+                    ? 300
+                    : results[0].name.toString().split(" ").length == 4
+                    ? 400
+                    : 400,
+                  240
+                )
+                .fontSize("16")
+                .text(results[0].serial_number, 375, 528)
                 .pipe(fs.createWriteStream(filePath))
                 .on("finish", function() {
                   fs.readFile(filePath, function(err, data) {
@@ -108,7 +77,7 @@ router.post("/download", (req, res) => {
                     res.send(data);
                   });
                 });
-
+              console.log(results[0].name.toString().split(" ").length);
               // Close PDF and write file.
               pdf.end();
             } else if (results[0].version == 1) {
